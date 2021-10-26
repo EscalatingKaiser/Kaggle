6c20e35463559a571734eb851029653f04331ccc
KM <escalatingkaiser@gmail.com>
2021-10-26 09:26:12 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,22 @@
-i=10
-print(i)
+class Solution(object):
+    def longest(self, s: str):
+        j = []  # temp list
+        c = 0  # temp combo
+        f = 0  # current max combo
+        for i in range(len(s)):
+            if s[i] in j:
+                if c > f:
+                    f = c  # update max combo
+                j.clear()
+                j.append(s[i])  # reset temp list
+                c = 1  # reset temp combo counts
+            else:
+                j.append(s[i])
+                c = c + 1
+        f = c
+        return f
+
+
+Solution.longest(self,"abcabc")
+
+
